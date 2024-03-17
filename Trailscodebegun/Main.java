545858a6308a26e662b708c51da373cfb3f9da75
RajasreeAvula <163755672+RajasreeAvula@users.noreply.github.com>
2024-03-17 22:01:31 +0530

    Add files via upload

---
+++ Trailscodebegun/Main.java
@@ -0,0 +1,14 @@
+package Trailscodebegun;
+
+public class Main {
+
+	private Main() {
+		System.out.println("codebegun");
+	}
+	
+	public static void main(String[] args) {
+		new Main();
+
+	}
+
+}
---
+++ Trailscodebegun/Quiz.java
@@ -0,0 +1,10 @@
+package Trailscodebegun;
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		String name=null;
+		System.out.println(name==null);
+	}
+
+}
