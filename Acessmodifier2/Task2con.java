2350c1bb363290b500bf1fa6a32a2888c494bea9
RajasreeAvula <163755672+RajasreeAvula@users.noreply.github.com>
2024-03-17 21:49:25 +0530

    Add files via upload

---
+++ Acessmodifier2/Task2con.java
@@ -0,0 +1,17 @@
+package Acessmodifier2;
+
+import Acessmodifier.Task1;
+
+public class Task2con {
+
+	public static void main(String[] args) {
+		Task1 obj=new Task1();
+
+		System.out.println(obj.c);
+		obj.m3();
+		
+		
+		
+	}
+
+}
